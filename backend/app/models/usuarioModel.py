607c3138e01edda998f883f68ae04047ebd3967f
EmanuelCJ <emanuelcastrillo95@gmail.com>
2025-08-28 23:18:46 -0300

    version: 1.1 28/08/25

--- backend/app/models/usuarioModel.py
+++ backend/app/models/usuarioModel.py
@@ -1,9 +1,9 @@
 class UsuarioModel:
-    id_usuario: int
-    nombre: str
-    apellido: str
-    rol: str
-    _contrasena: str
+    __id_usuario: int
+    __nombre: str
+    __apellido: str
+    __rol: str
+    __contrasena: str
 
     def __init__(self, id_usuario: int, nombre: str, apellido: str, rol: str, contrasena: str):
         self.id_usuario = id_usuario
@@ -12,5 +12,25 @@ class UsuarioModel:
         self.rol = rol
         self.contrasena = contrasena
 
-    def __mostrar__(self):
-        return f"<Usuario {self.nombre} {self.apellido}, Rol: {self.rol}>"
+    def __getID__(self):
+        return self.id_usuario
+    
+    def __getNombre__(self):
+        return self.nombre
+    
+    def __getApellido__(self):
+        return self.apellido
+    
+    def __getRol__(self):
+        return self.rol
+    
+
+
+usuario = UsuarioModel(1, "Juan", "Perez", "admin", "password123")  # Ejemplo de instancia
+
+print(usuario.__getNombre__())  # Llamada al método para obtener el nombre
+
+    
+
+
+
