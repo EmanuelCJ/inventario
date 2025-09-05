39bffbb7b2cdf9c3818c66be09416dfc959b947a
EmanuelCJ <emanuelcastrillo95@gmail.com>
2025-09-05 07:05:12 -0300

    version 2.0 05/09/25

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,3 +6,4 @@ def create_app():
     @app.route('/')
     def hello():
         return "Hello, World!"
+    return app
---
+++ backend/run.py
@@ -0,0 +1,8 @@
+#Del directorio app importamos la función create_app
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
