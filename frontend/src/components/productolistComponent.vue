4f19c04df545eab2f4a63887293b1675c02fa9f6
EmanuelCJ <emanuelcastrillo95@gmail.com>
2025-12-16 12:43:29 -0300

    version 3.3.8 16/12/25

--- frontend/src/components/productolistComponent.vue
+++ frontend/src/components/productolistComponent.vue
@@ -1,10 +1,41 @@
 <template>
-    <h1>lista de productos</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nihil magni fugit sapiente rerum? Repellat delectus dolor aspernatur assumenda at, dolorem ducimus non asperiores, perspiciatis tenetur cupiditate sunt exercitationem quos?</p>
+  <form @submit.prevent="handleSubmit">
+    <h1>LOGIN</h1>
+
+    <div>
+      <label>Username:</label>
+      <input type="text" v-model="username" />
+    </div>
+
+    <div>
+      <label>Password:</label>
+      <input type="password" v-model="password" />
+    </div>
+
+    <button type="submit">Login</button>
+  </form>
 </template>
+
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
 
+const username = ref("");
+const password = ref("");
+
+axios.defaults.withCredentials = true; // 👈 CLAVE
+
+const handleSubmit = async () => {
+  try {
+    const res = await axios.post("http://127.0.0.1:5000/auth/login", {
+      username: username.value,
+      password: password.value
+    });
+
+    console.log("Login OK", res.data);
+    // router.push("/dashboard")
+  } catch (err) {
+    console.error("Login error", err.response?.data);
+  }
+};
 </script>
-<style scoped>
-
-</style>
